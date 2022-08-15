51f37973020a2b016adc403a6c2bdff612a45c72
Luis Cubillos <luis.cubillos@exsis.loc>
2022-08-15 00:05:33 -0500

    estetica y footer

--- Proyecto final/js/cositas.js
+++ Proyecto final/js/cositas.js
@@ -2,6 +2,11 @@ class Cards extends HTMLElement{
     constructor(){
         super();
     }
+
+    connectedCallback(){
+        this.innerHTML = "<div>a</div>";
+        this.style.backgroundColor = "blue";
+    }
 }
 
 customElements.define("app-cards",Cards);
