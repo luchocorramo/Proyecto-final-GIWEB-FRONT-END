e6dad1520fb0e331af8336834ea4371749e398e4
Luis Cubillos <luis.cubillos@exsis.loc>
2022-08-15 21:35:19 -0500

    proyecto terminado

--- Proyecto final/js/cositas.js
+++ Proyecto final/js/cositas.js
@@ -1,11 +1,28 @@
 class Cards extends HTMLElement{
     constructor(){
         super();
+        this.nomIl = this.getAttribute('nomIl');
+        this.imag = this.getAttribute('imag');
+        this.linkIl = this.getAttribute('linkIl');
+    }
+
+
+    getTemplate(){
+        const template = document.createElement("template");
+        template.innerHTML =  `
+        <a href="${this.linkIl}" class="cards">
+
+            <img class="imagen" src="${this.imag}"> 
+            <div class="tittleCard">Ilustración: ${this.nomIl}</div>
+        
+        </a>
+        `;
+        
+        return template;
     }
 
     connectedCallback(){
-        this.innerHTML = "<div>a</div>";
-        this.style.backgroundColor = "blue";
+        this.appendChild(this.getTemplate().content.cloneNode(true));
     }
 }
 
